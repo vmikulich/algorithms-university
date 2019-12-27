8864027f7b2f97a6f5903c6097762a4fc9f0333f
VitalyMikulich <mr.wital1999@gmail.com>
2019-12-28 01:42:25 +0300

    update stable marriage problem

--- Lab-5(garph)/src/main/java/by/bsu/graph/EulerCycle.java
+++ Lab-5(garph)/src/main/java/by/bsu/graph/EulerCycle.java
@@ -48,18 +48,38 @@ public class EulerCycle {
         int vertex;
         while (!stack.isEmpty()){
             vertex = stack.peek();
+            System.out.println("vertex - " + vertex);
             for (int i: graph.getAdjacentList().keySet()) {
                 if (graph.getAdjacentList().get(vertex).contains(i)) {
                     stack.push(i);
+                    System.out.println("stack - " + stack);
                     graph.removeEdge(vertex ,i);
                     makeCycle(graph, stack);
                 }
             }
             if (!stack.isEmpty()){
                 cycle.add(stack.pop());
+                System.out.println(cycle);
+                System.out.println("res" + stack);
             } else {
                 break;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        ListGraph graph = new ListGraph();
+        Integer[] ver = new Integer[]{1, 2, 3, 4, 5};
+        graph.addVertices(ver);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 5);
+        EulerCycle eulercycle = new EulerCycle(graph);
+        eulercycle.findEulerCycle();
+        System.out.println(eulercycle.getCycle());
     }
 }
--- StableMarriageProblem/src/main/java/by/bsu/stableMarriageProblem/StableMarriageProblem.java
+++ StableMarriageProblem/src/main/java/by/bsu/stableMarriageProblem/StableMarriageProblem.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class StableMarriageProblem {
     public static void main(String[] args) {
 
-        int[][] priorities = {
+        Integer[][] priorities = {
                 {1,0,2,3,4},
                 {4,1,2,3,0},
                 {2,3,1,0,4},
@@ -13,49 +13,62 @@ public class StableMarriageProblem {
                 {2,0,4,1,3}
         };
 
-        int[][] estimations = {
-                {12,8,3,5,10},//{3,4,2,5,1}
+        Integer[][] estimations = {
+                {12,8,3,5,10},
                 {11,13,7,9,10},
                 {14,10,9,5,8},
                 {13,14,10,8,11},
                 {11,13,14,9,15}
         };
 
+        List<List<Integer>> estimationsCopy = new ArrayList<>();
+
+        for (int i = 0; i < estimations.length; i++) {
+            List<Integer> elem = new ArrayList<>();
+            elem.addAll(Arrays.asList(estimations[i]));
+            estimationsCopy.add(elem);
+            Arrays.sort(estimations[i]);
+            for (int j = 0; j < estimations[i].length; j++) {
+                Integer temp = estimations[i][j];
+                int index = estimationsCopy.get(i).indexOf(temp);
+                estimations[i][j] = index;
+            }
+            System.out.println(Arrays.asList(estimations[i]));
+        }
 
         Set<Integer> availableTasks = new HashSet <> ();
-        for (int i=0; i<estimations.length; i++) {
+        for (int i = 0; i < estimations.length; i++) {
             availableTasks.add(i);
         }
+
         Map<Integer, Integer> availableCoders = new HashMap <> ();
-        for (int i=0; i<priorities.length; i++) {
+        for (int i = 0; i < priorities.length; i++) {
             availableCoders.put(i, null);
         }
+
         while (availableTasks.size() > 0) {
             int currentTask = availableTasks.iterator().next();
-            SortedMap<Integer, Integer> sortedMap = new TreeMap<>();
-            for (int i = 0; i<estimations[currentTask].length; i++) {
-                sortedMap.put(estimations[currentTask][i],i);
-            }
-            for(Map.Entry<Integer,Integer> entry : sortedMap.entrySet()) {
-                Integer task = availableCoders.get(entry.getValue());
+
+            for(int entry : estimations[currentTask]) {
+                Integer task = availableCoders.get(entry);
                 if (task == null) {
-                    availableCoders.put(entry.getValue(), currentTask);
+                    availableCoders.put(entry, currentTask);
                     availableTasks.remove(currentTask);
                     break;
                 }
                 else {
                     int prefForTask = -1;
                     int prefForCurrentTask = -1;
-                    for (int k = 0; k < priorities[entry.getValue()].length; k++){
-                        if (priorities[entry.getValue()][k] == task) {
+                    for (int k = 0; k < priorities[entry].length; k++){
+                        if (priorities[entry][k] == task) {
                             prefForTask = k;
                         }
-                        if (priorities[entry.getValue()][k] == currentTask) {
+                        if (priorities[entry][k] == currentTask) {
                             prefForCurrentTask = k;
                         }
                     }
                     if (prefForCurrentTask < prefForTask){
-                        availableCoders.put(entry.getValue(), currentTask);
+                        availableCoders.put(entry, currentTask);
                         availableTasks.remove(currentTask);
                         availableTasks.add(task);
                         break;
@@ -70,10 +83,8 @@ public class StableMarriageProblem {
         while (itr.hasNext()){
             Map.Entry<Integer, Integer> entry = itr.next();
             System.out.println ("T-" + entry.getValue() + " Taken by C-" + entry.getKey());
-            totalEfficiency+=estimations[entry.getValue()][entry.getKey()];
+            totalEfficiency+=estimationsCopy.get(entry.getValue()).get(entry.getKey());
         }
         System.out.println("Total efficiency: " + totalEfficiency);
-
     }
-
 }
